9f47ad3d817a97d3f5322baca2fefeefce919a9c
Maximilian Kleinegger <mk145@illinois.edu>
2025-09-25 01:23:54 -0500

    first working impl

--- lab4/lab4.cu
+++ lab4/lab4.cu
@@ -1,25 +1,67 @@
 #include <wb.h>
 
-#define wbCheck(stmt)                                                     \
-  do {                                                                    \
-    cudaError_t err = stmt;                                               \
-    if (err != cudaSuccess) {                                             \
-      wbLog(ERROR, "CUDA error: ", cudaGetErrorString(err));              \
-      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
-      return -1;                                                          \
-    }                                                                     \
+#define wbCheck(stmt)                                        \
+  do                                                         \
+  {                                                          \
+    cudaError_t err = stmt;                                  \
+    if (err != cudaSuccess)                                  \
+    {                                                        \
+      wbLog(ERROR, "CUDA error: ", cudaGetErrorString(err)); \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);            \
+      return -1;                                             \
+    }                                                        \
   } while (0)
 
 //@@ Define any useful program-wide constants here
+#define FILTER_DIM 3
+#define TILE_WIDTH 3
 
 //@@ Define constant memory for device kernel here
+__constant__ float filter_c[FILTER_DIM][FILTER_DIM][FILTER_DIM];
 
 __global__ void conv3d(float *input, float *output, const int z_size,
-                       const int y_size, const int x_size) {
+                       const int y_size, const int x_size)
+{
   //@@ Insert kernel code here
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int tz = threadIdx.z;
+
+  unsigned int col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+  unsigned int row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+  unsigned int depth = blockIdx.z * TILE_WIDTH + threadIdx.z;
+
+  __shared__ float tile[TILE_WIDTH + FILTER_DIM - 1][TILE_WIDTH + FILTER_DIM - 1][TILE_WIDTH + FILTER_DIM - 1];
+
+  int row_i = row - (FILTER_DIM / 2);
+  int col_i = col - (FILTER_DIM / 2);
+  int depth_i = depth - (FILTER_DIM / 2);
+
+  if ((row_i >= 0) && (row_i < y_size) && (col_i >= 0) && (col_i < x_size) && (depth_i >= 0) && (depth_i < z_size)) {
+    tile[tz][ty][tx] += input[(depth_i * TILE_WIDTH * y_size) + (row_i * x_size) + col_i];
+  } else {
+    tile[tz][ty][tx] = 0.0f;
+  }
+
+  __syncthreads();
+
+  float output_value = 0.0f;
+  if (tz < TILE_WIDTH && ty < TILE_WIDTH && tx < TILE_WIDTH) {
+    for (int z = 0; z < FILTER_DIM; z++) {
+      for (int y = 0; y < FILTER_DIM; y++) {
+        for (int x = 0; x < FILTER_DIM; x++) {
+          output_value += tile[tz + z][ty + y][tx + x] * filter_c[z][y][x];
+        }
+      }
+    }
+    if (row < y_size && col < x_size && depth < z_size) {
+      output[(depth * y_size * x_size) + (row * x_size) + col] = output_value;
+    }
+  }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   wbArg_t args;
   int z_size;
   int y_size;
@@ -29,6 +71,8 @@ int main(int argc, char *argv[]) {
   float *hostKernel;
   float *hostOutput;
   //@@ Initial deviceInput and deviceOutput here.
+  float *deviceInput;
+  float *deviceOutput;
 
   args = wbArg_read(argc, argv);
 
@@ -46,34 +90,31 @@ int main(int argc, char *argv[]) {
   assert(z_size * y_size * x_size == inputLength - 3);
   assert(kernelLength == 27);
 
-
   //@@ Allocate GPU memory here
   // Recall that inputLength is 3 elements longer than the input data
-  // because the first  three elements were the dimensions
-
-
+  // because the first three elements were the dimensions
+  wbCheck(cudaMalloc((void **)&deviceInput, (inputLength - 3) * sizeof(float)));
+  wbCheck(cudaMalloc((void **)&deviceOutput, (inputLength - 3) * sizeof(float)));
 
   //@@ Copy input and kernel to GPU here
   // Recall that the first three elements of hostInput are dimensions and
-  // do
-  // not need to be copied to the gpu
-
-
+  // do not need to be copied to the gpu
+  wbCheck(cudaMemcpy(deviceInput, hostInput + 3, (inputLength - 3) * sizeof(float), cudaMemcpyHostToDevice));
+  wbCheck(cudaMemcpyToSymbol(filter_c, hostKernel, kernelLength * sizeof(float), 0, cudaMemcpyHostToDevice));
 
   //@@ Initialize grid and block dimensions here
+  dim3 dimBlock(TILE_WIDTH + (FILTER_DIM - 1), TILE_WIDTH + (FILTER_DIM - 1), TILE_WIDTH + (FILTER_DIM - 1));
+  dim3 dimGrid(ceil(x_size / (1.0 * TILE_WIDTH)), ceil(y_size / (1.0 * TILE_WIDTH)), ceil(z_size / (1.0 * TILE_WIDTH)));
 
   //@@ Launch the GPU kernel here
+  conv3d<<<dimGrid, dimBlock>>>(deviceInput, deviceOutput, z_size, y_size, x_size);
 
   cudaDeviceSynchronize();
-
-
 
   //@@ Copy the device memory back to the host here
   // Recall that the first three elements of the output are the dimensions
   // and should not be set here (they are set below)
-
-
-
+  wbCheck(cudaMemcpy(hostOutput + 3, deviceOutput, (inputLength - 3) * sizeof(float), cudaMemcpyDeviceToHost));
 
   // Set the output dimensions for correctness checking
   hostOutput[0] = z_size;
@@ -81,12 +122,16 @@ int main(int argc, char *argv[]) {
   hostOutput[2] = x_size;
   wbSolution(args, hostOutput, inputLength);
 
-  //@@ Free device memory
+  // for (int i = 0; i < 10; i++) {
+  //   printf("%f ", hostOutput[i]);
+  // }
 
+  //@@ Free device memory
+  wbCheck(cudaFree(deviceInput));
+  wbCheck(cudaFree(deviceOutput));
 
   // Free host memory
   free(hostInput);
   free(hostOutput);
   return 0;
 }
-
